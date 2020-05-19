831e337f1b6e0827101eaa0be8b81d86a4eb331b
lalolipc <mardelplatarocio@gmail.com>
2020-05-19 20:36:17 -0300

    create spring boots directories

---
+++ src/main/java/com/utn/PhoneLines/PhoneLinesApplication.java
@@ -0,0 +1,13 @@
+package com.utn.PhoneLines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneLinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneLinesApplication.class, args);
+	}
+
+}
